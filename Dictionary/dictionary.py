26b57faedc1b610977dcb182fa4e44c893dab985
FlaviusMazilu <maziluflavius@gmail.com>
2024-05-20 18:34:07 +0300

    use black linter
    Signed-off-by: FlaviusMazilu <maziluflavius@gmail.com>

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -1,7 +1,10 @@
 """docstring here"""
+
 import json
 from difflib import get_close_matches
+
 DATA = json.load(open("data.json"))
+
 
 def translate(word):
     word = word.lower()
@@ -12,7 +15,7 @@ def translate(word):
     if word.upper() in DATA:
         return DATA[word.upper()]
     if len(get_close_matches(word, DATA.keys())) > 0:
-        print("did you mean %s instead" %get_close_matches(word, DATA.keys())[0])
+        print("did you mean %s instead" % get_close_matches(word, DATA.keys())[0])
         decide = input("press y for yes or n for no: ")
         if decide == "y":
             return DATA[get_close_matches(word, DATA.keys())[0]]
